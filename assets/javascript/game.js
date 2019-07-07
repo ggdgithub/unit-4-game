2365dc06f91604a64b872ef97479be10dcfa6d43
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-07-07 15:04:40 -0700

    pseudocoded game

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Pseudocode
+    // Variables
+        // Goal Number Variable
+            // Randomly Chosen 19-120
+            // Displayed on Webpage
+        // Crystal Buttons
+            // Randomly Chosen 1-12
+            // value hidden until clicked
+            // value shown on page
+            // addive properties
+        // Wins
+        // Losses
+    // Game loop
+        // player given number
+        // player clicks on crystal
+        // player finds out value of crystal as they add up
+        // Win condition
+            // if player score > given number || player quits
+                // player loses
+                // track losses++
+            // else if player score === given number
+                // player wins
+                // track wins++
+            
