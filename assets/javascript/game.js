f7a8b6046ae8d3df8704283b48dedb0ca8721672
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-07-07 18:36:21 -0700

    pseudocoded what should happen when player passes goal number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,13 @@
-// Pseudocode
-    // Variables
+    // DOESN'T CHANGE OVER TIME
 var goalNumber = Math.floor(Math.random() * (120 - 19) + 19);
-
 var crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
 var crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
 var crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
 var crystal4 = Math.floor(Math.random() * (12 - 1) + 1);
 
+// CHANGES OVER TIME
 var playerNumber = 0;
-
 var wins = 0;
-
 var losses = 0;
 
 console.log(crystal1);
@@ -18,6 +15,16 @@ console.log(crystal2);
 console.log(crystal3);
 console.log(crystal4);
 console.log(goalNumber);
+
+if(playerNumber > goalNumber) {
+    // say player lost
+    // losses++
+}
+
+else if(playerNumber === goalNumber) {
+    // say player won
+    // wins++
+}
         // X Goal Number Variable
             // X Randomly Chosen 19-120
             // X Displayed on Webpage
