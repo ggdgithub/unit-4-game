a2873aacdf8a51380db2dcf6ce765b781214d4ae
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-07-07 18:18:19 -0700

    variables output proper random numbers according to console.log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,33 @@
 // Pseudocode
     // Variables
-var goalNumber;
-var crystal = [
-    "one",
-    "two",
-    "three",
-    "four"
-];
+var goalNumber = Math.floor(Math.random() * (120 - 19) + 19);
+
+var crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal4 = Math.floor(Math.random() * (12 - 1) + 1);
+
 var playerNumber = 0;
+
 var wins = 0;
+
 var losses = 0;
 
-        // Goal Number Variable
-            // Randomly Chosen 19-120
-            // Displayed on Webpage
-        // Crystal Buttons
-            // Randomly Chosen 1-12
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+console.log(goalNumber);
+        // X Goal Number Variable
+            // X Randomly Chosen 19-120
+            // X Displayed on Webpage
+        // X Crystal Buttons
+            // X Randomly Chosen 1-12
             // value hidden until clicked
             // value shown on page
             // addive properties
-        // Wins
-        // Losses
+        // X Wins
+        // X Losses
     // Game loop
         // player given number
         // player clicks on crystal
@@ -32,4 +39,3 @@ var losses = 0;
             // else if player score === given number
                 // player wins
                 // track wins++
-            
